fa23eb50b37b1dfa68b510b2c911062313ad324c
Davi Siqueira <davi.mlp@gmail.com>
2024-05-09 14:55:48 +0100

    testing sprites

--- mlx.c
+++ mlx.c
@@ -27,13 +27,17 @@ void	start_mlx(t_game *so_long)
 
 void	set_sprites(t_game *so_long)
 {
-	so_long->sprites = malloc(sizeof(t_sprite));
+	so_long->sprites = malloc(5 * sizeof(t_sprite));
 	if (!(so_long->sprites))
 	{
 		ft_printf("Couldn't allocate sprites");
 		return ;
 	}
-	so_long->sprites->img = mlx_xpm_file_to_image(so_long->mlx, FW, &(so_long->sprites->width), &(so_long->sprites->height));
+	so_long->sprites[W1]->img = mlx_xpm_file_to_image(so_long->mlx, FW, &(so_long->sprites[W1]->width), &(so_long->sprites[W1]->height));
+	so_long->sprites[B1]->img = mlx_xpm_file_to_image(so_long->mlx, FB, &(so_long->sprites[B1]->width), &(so_long->sprites[B1]->height));
+	so_long->sprites[E1]->img = mlx_xpm_file_to_image(so_long->mlx, FE, &(so_long->sprites[E1]->width), &(so_long->sprites[E1]->height));
+/*	so_long->sprites[C1]->img = mlx_xpm_file_to_image(so_long->mlx, FC, &(so_long->sprites[C1]->width), &(so_long->sprites[C1]->height));
+	so_long->sprites[P1]->img = mlx_xpm_file_to_image(so_long->mlx, FP, &(so_long->sprites[P1]->width), &(so_long->sprites[P1]->height));*/
 }
 
 void	see_map(t_game *so_long)
@@ -52,5 +56,13 @@ void	see_map(t_game *so_long)
 void	put_sprites(t_game *so_long, int x, int y)
 {
 	if (so_long->map[y][x] == '1')
-		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites->img, so_long->sprites->width * x, so_long->sprites->height * y);
+		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites[W1]->img, so_long->sprites[W1]->width * x, so_long->sprites[W1]->height * y);
+	else if (so_long->map[y][x] == '0')
+		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites[B1]->img, so_long->sprites[B1]->width * x, so_long->sprites[B1]->height * y);
+	else if (so_long->map[y][x] == 'E')
+		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites[E1]->img, so_long->sprites[E1]->width * x, so_long->sprites[E1]->height * y);
+/*	else if (so_long->map[y][x] == 'C')
+		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites[C1]->img, so_long->sprites[C1]->width * x, so_long->sprites[C1]->height * y);
+	else if (so_long->map[y][x] == 'P')
+		mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->sprites[P1]->img, so_long->sprites[P1]->width * x, so_long->sprites[P1]->height * y);*/
 }
